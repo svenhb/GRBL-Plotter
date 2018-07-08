449a14c95a1554166e976fd4427fec0ef312fafa
Sven <svenhb@gmx.de>
2018-07-08 22:32:51 +0200

    Update 1.1.6.4

--- GRBL-Plotter/Properties/AssemblyInfo.cs
+++ GRBL-Plotter/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.6.3")]
-[assembly: AssemblyFileVersion("1.1.6.3")]
+[assembly: AssemblyVersion("1.1.6.4")]
+[assembly: AssemblyFileVersion("1.1.6.4")]
 [assembly: NeutralResourcesLanguage("en")]
 
