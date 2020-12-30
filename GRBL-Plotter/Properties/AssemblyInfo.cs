4181215e64c913de70e051ac1d608dd44a15d928
Sven <svenhb@gmx.de>
2020-12-30 22:18:44 +0100

    Version 1.5.4.2

--- GRBL-Plotter/Properties/AssemblyInfo.cs
+++ GRBL-Plotter/Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.4.1")]
-[assembly: AssemblyFileVersion("1.5.4.1")]
+[assembly: AssemblyVersion("1.5.4.2")]
+[assembly: AssemblyFileVersion("1.5.4.2")]
 [assembly: NeutralResourcesLanguage("en")]
 
