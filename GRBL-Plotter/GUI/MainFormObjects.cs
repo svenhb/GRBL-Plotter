f4a99cf150c3fee1e479aa6037efdd8c7c6f101e
Sven <svenhb@gmx.de>
2023-12-21 08:56:58 +0100

    Version 1.7.1.0.a

--- GRBL-Plotter/GUI/MainFormObjects.cs
+++ GRBL-Plotter/GUI/MainFormObjects.cs
@@ -50,7 +50,7 @@ namespace GrblPlotter
 
     public static class MyApplication
     {
-        private static readonly string VersionAddOn = "";
+        private static readonly string VersionAddOn = ".a";
 
         public static string GetVersion()
         { return System.Windows.Forms.Application.ProductVersion.ToString() + VersionAddOn; }
